e737e33871eb6e1d1dd741b65b9c57a2cce7a66e
alyasin <mahdi.alyasin@smartmicro.de>
2025-02-27 10:59:35 +0100

    Day 5.0

--- Folien/Notebooks/Code-Along/Woche 1/bad_names_01.py
+++ Folien/Notebooks/Code-Along/Woche 1/bad_names_01.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+UNFLAGGED = 0
+FLAGGED = 1
+
 
 @dataclass
 class MineSweeper:  # type: ignore
@@ -8,14 +11,14 @@ class MineSweeper:  # type: ignore
     def get_flagged_cells(self):
         flagged_cells = []
         for cell in self.board:
-            if cell[1] == 1:
+            if cell[1] == FLAGGED:
                 flagged_cells.append(cell)
         return flagged_cells
 
 
 if __name__ == "__main__":
-    thing = MineSweeper([(i, 0, 0) for i in range(64)])
-    thing.board[2] = (2, 1, 0)
+    thing = MineSweeper([(i, UNFLAGGED, UNFLAGGED) for i in range(64)])
+    thing.board[2] = (2, FLAGGED, 0)
 
     assert thing.get_flagged_cells() == [(2, 1, 0)]
     print(thing.get_flagged_cells())
